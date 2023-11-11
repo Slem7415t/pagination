59d65033441902ae7a5c593a1dae5e98d7925fa8
Slem7415t <xalexsemx@yandex.ru>
2023-11-11 15:24:23 +0300

    version_1.3

--- js/script.js
+++ js/script.js
@@ -1,77 +1,3 @@
-async function getData() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await response.json();
-    return data;
-} //получаем посты
-
-async function main() {
-    const postsData = await getData(); // контент arrData
-    let currentPage = 1; // номер-начальной-страницы page
-    let rows = 10; // количество элементов на странице rowPerPage
-
-    function displayList (arrData, rowPerPage, page) {
-        const postsEl = document.querySelector('.posts'); // получаем div-контента
-        postsEl.innerHTML = ""; // чистим div-контента от предыдущего контента,
-        // перед размещением контента чтоб не накапливался
-        page--; // уменьшаем номер-начальной-страницы т.к массив у нас с нуля
-
-        const start = rowPerPage * page; // начало среза страницы
-        const end = start + rowPerPage; // конец среза страницы
-        const paginatedData = arrData.slice(start, end); // срез контента
-        // который будет отображен на странице
-
-        paginatedData.forEach((el) => {
-            const postEl = document.createElement("div"); // создаем div-элемента
-            postEl.classList.add("post"); // добавляем класс div-элемента 
-            postEl.innerText = `${el.title}`; // добавляем в div-элемент контент
-            postsEl.appendChild(postEl); // добавляем div-элемента в div-контента
-        }); // циклом добавляем все div-элемента в div-контента
-    } // выводит контент в пределах среза
-
-    function displayPagination (arrData, rowPerPage) {
-        const paginationEl = document.querySelector('.pagination'); // получаем div-кнопок
-        const pagesCount = Math.ceil(arrData.length / rowPerPage); // получаем количество кнопок в блоке
-        const ulEl = document.createElement("ul"); // создаем контейнер-кнопок
-        ulEl.classList.add('pagination__list') // добавляем класс контейнеру-кнопок
-
-        for (let i = 0; i < pagesCount; i++) {
-            const liEl = displayPaginationBtn(i + 1); // вызываем кнопку и присваиваем ей номер
-            ulEl.appendChild(liEl) // добавляем кнопку в контейнер-кнопок
-        } // циклом присваиваем каждой кнопке значение и добавляем её в контейнер-кнопок
-
-        paginationEl.appendChild(ulEl) // добавляем контейнер-кнопок в div-кнопок
-    } // выводит блок с кнопками 
-
-    function displayPaginationBtn (page) {
-        const liEl = document.createElement("li"); // создаем кнопку
-        liEl.classList.add('pagination__item'); // присваиваем кнопке класс 
-        liEl.innerText = page // присваиваем кнопке значение аргумента
-
-        if (currentPage == page) liEl.classList.add('pagination__item_active'); //условие
-        //если значение кнопки и номер-начальной-страницы совпадают ей добавляется класс active
-
-        // событие(при нажатии на кнопку)
-        liEl.addEventListener('click', () => {
-            currentPage = page; // номеру-начальной-страницы присваивается значение-кнопки
-            displayList(postsData, rows, currentPage); // получаем контент в пределах среза
-
-            let currentItemLi = document.querySelector('li.pagination__item_active'); // получаем
-            // кнопку с классом active
-            currentItemLi.classList.remove('pagination__item_active'); // убираем класс active,
-            // с полученной кнопки
-            liEl.classList.add('pagination__item_active'); // добавляем класс active,
-            // кнопке на которую нажали
-        }) // вешаем событие на кнопку
-
-        return liEl; // возвращаем кнопку в функцию
-    } // создаем кнопку
-
-    displayList(postsData, rows, currentPage); // вызываем контент в пределах среза
-    displayPagination(postsData, rows); // вызываем блок с кнопками
-} // блок с пагинацией
-
-main(); // вызываем блок с пагинацией
-
 const cards = [
     {
         "id": 1,
@@ -90,6 +16,240 @@ const cards = [
         "images" : "images/cards/image-3.jpg",
         "title" : "picture-3-title",
         "description" : "picture-3-description"
+    },
+    {
+        "id": 4,
+        "images" : "images/cards/image-4.jpg",
+        "title" : "picture-4-title",
+        "description" : "picture-4-description"
+    },
+    {
+        "id": 5,
+        "images" : "images/cards/image-5.jpg",
+        "title" : "picture-5-title",
+        "description" : "picture-5-description"
+    },
+    {
+        "id": 6,
+        "images" : "images/cards/image-6.jpg",
+        "title" : "picture-6-title",
+        "description" : "picture-6-description"
+    },
+    {
+        "id": 7,
+        "images" : "images/cards/image-7.jpg",
+        "title" : "picture-7-title",
+        "description" : "picture-7-description"
+    },
+    {
+        "id": 8,
+        "images" : "images/cards/image-8.jpg",
+        "title" : "picture-8-title",
+        "description" : "picture-8-description"
+    },
+    {
+        "id": 9,
+        "images" : "images/cards/image-9.jpg",
+        "title" : "picture-9-title",
+        "description" : "picture-9-description"
+    },
+    {
+        "id": 10,
+        "images" : "images/cards/image-10.jpg",
+        "title" : "picture-10-title",
+        "description" : "picture-10-description"
+    },
+    {
+        "id": 11,
+        "images" : "images/cards/image-11.jpg",
+        "title" : "picture-11-title",
+        "description" : "picture-11-description"
+    },
+    {
+        "id": 12,
+        "images" : "images/cards/image-12.jpg",
+        "title" : "picture-12-title",
+        "description" : "picture-12-description"
+    },
+    {
+        "id": 13,
+        "images" : "images/cards/image-13.jpg",
+        "title" : "picture-13-title",
+        "description" : "picture-13-description"
+    },
+    {
+        "id": 14,
+        "images" : "images/cards/image-14.jpg",
+        "title" : "picture-14-title",
+        "description" : "picture-14-description"
+    },
+    {
+        "id": 15,
+        "images" : "images/cards/image-15.jpg",
+        "title" : "picture-15-title",
+        "description" : "picture-15-description"
+    },
+    {
+        "id": 16,
+        "images" : "images/cards/image-16.jpg",
+        "title" : "picture-16-title",
+        "description" : "picture-16-description"
+    },
+    {
+        "id": 17,
+        "images" : "images/cards/image-17.jpg",
+        "title" : "picture-17-title",
+        "description" : "picture-17-description"
+    },
+    {
+        "id": 18,
+        "images" : "images/cards/image-18.jpg",
+        "title" : "picture-18-title",
+        "description" : "picture-18-description"
+    },
+    {
+        "id": 19,
+        "images" : "images/cards/image-19.jpg",
+        "title" : "picture-19-title",
+        "description" : "picture-19-description"
+    },
+    {
+        "id": 20,
+        "images" : "images/cards/image-20.jpg",
+        "title" : "picture-20-title",
+        "description" : "picture-20-description"
+    },
+    {
+        "id": 21,
+        "images" : "images/cards/image-21.jpg",
+        "title" : "picture-21-title",
+        "description" : "picture-21-description"
+    },
+    {
+        "id": 22,
+        "images" : "images/cards/image-22.jpg",
+        "title" : "picture-22-title",
+        "description" : "picture-22-description"
+    },
+    {
+        "id": 23,
+        "images" : "images/cards/image-23.jpg",
+        "title" : "picture-23-title",
+        "description" : "picture-23-description"
+    },
+    {
+        "id": 24,
+        "images" : "images/cards/image-24.jpg",
+        "title" : "picture-24-title",
+        "description" : "picture-24-description"
+    },
+    {
+        "id": 25,
+        "images" : "images/cards/image-25.jpg",
+        "title" : "picture-25-title",
+        "description" : "picture-25-description"
+    },
+    {
+        "id": 26,
+        "images" : "images/cards/image-26.jpg",
+        "title" : "picture-26-title",
+        "description" : "picture-26-description"
+    },
+    {
+        "id": 27,
+        "images" : "images/cards/image-27.jpg",
+        "title" : "picture-27-title",
+        "description" : "picture-27-description"
+    },
+    {
+        "id": 28,
+        "images" : "images/cards/image-28.jpg",
+        "title" : "picture-28-title",
+        "description" : "picture-28-description"
+    },
+    {
+        "id": 29,
+        "images" : "images/cards/image-29.jpg",
+        "title" : "picture-29-title",
+        "description" : "picture-29-description"
+    },
+    {
+        "id": 30,
+        "images" : "images/cards/image-30.jpg",
+        "title" : "picture-30-title",
+        "description" : "picture-30-description"
+    },
+    {
+        "id": 31,
+        "images" : "images/cards/image-31.jpg",
+        "title" : "picture-31-title",
+        "description" : "picture-31-description"
+    },
+    {
+        "id": 32,
+        "images" : "images/cards/image-32.jpg",
+        "title" : "picture-32-title",
+        "description" : "picture-32-description"
+    },
+    {
+        "id": 33,
+        "images" : "images/cards/image-33.jpg",
+        "title" : "picture-33-title",
+        "description" : "picture-33-description"
+    },
+    {
+        "id": 34,
+        "images" : "images/cards/image-34.jpg",
+        "title" : "picture-34-title",
+        "description" : "picture-34-description"
+    },
+    {
+        "id": 35,
+        "images" : "images/cards/image-35.jpg",
+        "title" : "picture-35-title",
+        "description" : "picture-35-description"
+    },
+    {
+        "id": 36,
+        "images" : "images/cards/image-36.jpg",
+        "title" : "picture-36-title",
+        "description" : "picture-36-description"
+    },
+    {
+        "id": 37,
+        "images" : "images/cards/image-37.jpg",
+        "title" : "picture-37-title",
+        "description" : "picture-37-description"
+    },
+    {
+        "id": 38,
+        "images" : "images/cards/image-38.jpg",
+        "title" : "picture-38-title",
+        "description" : "picture-38-description"
+    },
+    {
+        "id": 39,
+        "images" : "images/cards/image-39.jpg",
+        "title" : "picture-39-title",
+        "description" : "picture-39-description"
+    },
+    {
+        "id": 40,
+        "images" : "images/cards/image-40.jpg",
+        "title" : "picture-40-title",
+        "description" : "picture-40-description"
+    },
+    {
+        "id": 41,
+        "images" : "images/cards/image-41.jpg",
+        "title" : "picture-41-title",
+        "description" : "picture-41-description"
+    },
+    {
+        "id": 42,
+        "images" : "images/cards/image-42.jpg",
+        "title" : "picture-42-title",
+        "description" : "picture-42-description"
     }
 ];
 
@@ -101,54 +261,110 @@ previewBtn.onclick = () => {
     document.querySelector('html').classList.remove('no-scroll');
 }
 
-function createCard(selector, array) {
-    for (let i = 0; i <= array.length; i++) {
-        let item = document.createElement('div');
-        item.classList.add('card');
-        
-        item.innerHTML = `
-        <div class="card-img">
-            <img src="${array[i]['images']}" alt="image-${array[i]['id']}" />
-        </div>  
-        <div class="card-content">
-            <h3 class="card__title">${array[i]["title"]}</h3>
-            <p class="card__description">${array[i]["description"]}</p>
-            <button class="card__button btn" type="button" data-modal-btn="image-${array[i]['id']}">Подробнее</button>
-        </div>
-        `;
+function main() {
+        const postsData = cards; // контент arrData
+        let currentPage = 1; // номер-начальной-страницы page
+        let rows = 10; // количество элементов на странице rowPerPage
+    
+        function displayList (arrData, rowPerPage, page) {
+            const postsEl = document.querySelector('.cards'); // получаем div-контента
+            postsEl.innerHTML = ""; // чистим div-контента от предыдущего контента,
+            // перед размещением контента чтоб не накапливался
+            page--; // уменьшаем номер-начальной-страницы т.к массив у нас с нуля
+    
+            const start = rowPerPage * page; // начало среза страницы
+            const end = start + rowPerPage; // конец среза страницы
+            const paginatedData = arrData.slice(start, end); // срез контента
+            // который будет отображен на странице
+    
+            paginatedData.forEach((el) => {
+                let item = document.createElement('div');
+                item.classList.add('card');
+    
+                item.innerHTML = `
+                <div class="card-img">
+                    <img src="${el.images}" alt="image-${el.id}" />
+                </div>  
+                <div class="card-content">
+                    <h3 class="card__title">${el.title}</h3>
+                    <p class="card__description">${el.description}</p>
+                    <button class="card__button btn" type="button" data-modal-btn="image-${el.id}">Подробнее</button>
+                </div>
+                `;
+    
+                document.querySelector('.cards').append(item);
+    
+                const preview = document.querySelector('.preview');
+    
+                document.querySelector(`[data-modal-btn="image-${el.id}"]`).addEventListener('click', function(){
+    
+                    
+                    let modal = document.querySelector(`[data-modal-img="image-${el.id}"]`);
+    
+                    let imgs = document.querySelectorAll(".preview__img img");    
+    
+                    for (let itemImgs of imgs) {
+                        itemImgs.style.display = "none";
+                    }
+    
+                    preview.style.display = "flex";
+                    modal.style.display = "flex";
+                    document.querySelector('html').classList.add('no-scroll');
+                });
+    
+                let itemImg = document.createElement('div');
+                itemImg.classList.add('preview__img');
+    
+                itemImg.innerHTML = `
+                    <img src="${el.images}" alt="image-${el.id}" data-modal-img="image-${el.id}" />
+                `;
+    
+                document.querySelector('.preview').append(itemImg);
 
-        document.querySelector(selector).append(item);
+            }); // циклом добавляем все div-элемента в div-контента
 
-        const preview = document.querySelector('.preview');
-
-        let btns = document.querySelectorAll("*[data-modal-btn]");
-
-        btns[i].addEventListener('click', function(){
-
-            let name = btns[i].getAttribute('data-modal-btn');
-            let modal = document.querySelector("[data-modal-img='" + name + "']");
-
-            let imgs = document.querySelectorAll(".preview__img img");    
-            
-            for (let itemImgs of imgs) {
-                itemImgs.style.display = "none";
-            }
-
-            preview.style.display = "flex";
-            modal.style.display = "flex";
-            document.querySelector('html').classList.add('no-scroll');
-        });
-
-        let itemImg = document.createElement('div');
-        itemImg.classList.add('preview__img');
-
-        itemImg.innerHTML = `
-            <img src="${array[i]['images']}" alt="image-${array[i]['id']}" data-modal-img="image-${array[i]['id']}" />
-        `;
-
-        document.querySelector('.preview').append(itemImg);
-
-    }
-
-}
-createCard('.cards', cards);
+        } // выводит контент в пределах среза
+    
+        function displayPagination (arrData, rowPerPage) {
+            const paginationEl = document.querySelector('.pagination'); // получаем div-кнопок
+            const pagesCount = Math.ceil(arrData.length / rowPerPage); // получаем количество кнопок в блоке
+            const ulEl = document.createElement("ul"); // создаем контейнер-кнопок
+            ulEl.classList.add('pagination__list') // добавляем класс контейнеру-кнопок
+    
+            for (let i = 0; i < pagesCount; i++) {
+                const liEl = displayPaginationBtn(i + 1); // вызываем кнопку и присваиваем ей номер
+                ulEl.appendChild(liEl) // добавляем кнопку в контейнер-кнопок
+            } // циклом присваиваем каждой кнопке значение и добавляем её в контейнер-кнопок
+    
+            paginationEl.appendChild(ulEl) // добавляем контейнер-кнопок в div-кнопок
+        } // выводит блок с кнопками 
+    
+        function displayPaginationBtn (page) {
+            const liEl = document.createElement("li"); // создаем кнопку
+            liEl.classList.add('pagination__item'); // присваиваем кнопке класс 
+            liEl.innerText = page // присваиваем кнопке значение аргумента
+    
+            if (currentPage == page) liEl.classList.add('pagination__item_active'); //условие
+            //если значение кнопки и номер-начальной-страницы совпадают ей добавляется класс active
+    
+            // событие(при нажатии на кнопку)
+            liEl.addEventListener('click', () => {
+                currentPage = page; // номеру-начальной-страницы присваивается значение-кнопки
+                displayList(postsData, rows, currentPage); // получаем контент в пределах среза
+    
+                let currentItemLi = document.querySelector('li.pagination__item_active'); // получаем
+                // кнопку с классом active
+                currentItemLi.classList.remove('pagination__item_active'); // убираем класс active,
+                // с полученной кнопки
+                liEl.classList.add('pagination__item_active'); // добавляем класс active,
+                // кнопке на которую нажали
+            }) // вешаем событие на кнопку
+    
+            return liEl; // возвращаем кнопку в функцию
+        } // создаем кнопку
+    
+        displayList(postsData, rows, currentPage); // вызываем контент в пределах среза
+        displayPagination(postsData, rows); // вызываем блок с кнопками
+    } // блок с пагинацией
+    
+    main(); // вызываем блок с пагинацией
